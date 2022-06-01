3e01c4247f2438edf6603c512f478ea4fd58b421
dariusbarbus <simpson.dario@gmail.com>
2022-06-01 14:43:11 -0500

    Update

---
+++ README.md
@@ -0,0 +1,7 @@
+1. Sophia script
+2. Install [Sunvalley](https://github.com/mrkenhoo/sunvalley-srw) and remove all UWP APPS
+3. Restore the store and terminal
+    -  Get-AppXPackage WindowsStore -AllUsers  Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)AppXManifest.xml”}
+    - Get-AppXPackage WindowsTerminal -AllUsers  Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)AppXManifest.xml”}
+4. Install Terminal using instructions
+5. Install Office
--- installer.ps1
+++ installer.ps1
@@ -1,7 +1,3 @@
-#region Protocols
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$ProgressPreference = 'Continue'
-#endregion
 
 #region Date and Time preferences
 Set-ItemProperty "HKCU:\HKEY_CURRENT_USER\Control Panel\International\" -Name "iFirstDayOfWeek" -Value "0"
@@ -15,41 +11,6 @@ Set-Itemproperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advan
 
 #region Disable Snap Layouts in Taskbar
 Set-Itemproperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "EnableSnapAssistFlyout" -Value 0
-#endregion
-
-#region Variables First Script
-$source1 = "https://github.com/dariusbarbus/FORK-OF-Sophia-Script-for-Windows/archive/refs/heads/master.zip"
-$output1 = "C:\setup\script\dbsophia.zip"
-#endregion
-
-#region Variables Second Script
-$source2 = "https://github.com/mrkenhoo/sunvalley-srw/releases/download/0.0.12.5/sunvalley-srw-setup.exe"
-$output2 = "C:\setup\script\sunvalley-srw.exe"
-#endregion
-
-#region New directory
-$path = "C:\setup\script"
-New-Item -ItemType Directory -Path $path
-#endregion
-
-#region Scripts Download
-Write-Host "Downloading Scripts"
-Invoke-WebRequest -Uri $source1 -OutFile $output1 -Method Get
-Expand-Archive -Path $output1 -DestinationPath $path -Force
-Invoke-WebRequest -Uri $source2 -OutFile $output2
-#endregion
-
-Invoke-WebRequest -Uri $source2 -OutFile $output2
-
-#region Run First Script
-Invoke-Expression "&'C:\setup\script\FORK-OF-Sophia-Script-for-Windows-master\Sophia Script\Sophia Script for Windows 11\Sophia.ps1'"
-#endregion
-
-
-
-#region RESTORE WINDOWS STORE AND TERMINAL
-#Get-AppXPackage WindowsStore -AllUsers  Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)AppXManifest.xml”}
-#Get-AppXPackage WindowsTerminal -AllUsers  Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)AppXManifest.xml”}
 #endregion
 
 # #PROGRAM LIST
